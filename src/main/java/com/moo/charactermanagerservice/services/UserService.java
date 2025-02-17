aa363b0af36243b5fcf7cc8a2e749f4d9ac5f9c1
jvilla1991 <villajeremy1991@gmail.com>
2025-02-17 17:24:25 -0500

    Added error handling to User Service

--- src/main/java/com/moo/charactermanagerservice/services/UserService.java
+++ src/main/java/com/moo/charactermanagerservice/services/UserService.java
@@ -5,6 +5,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.http.*;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
@@ -19,17 +21,26 @@ public class UserService {
     }
 
     public User getUserDetails(String authorizationHeader) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.set(HttpHeaders.AUTHORIZATION, authorizationHeader);
-        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+        try{
+            HttpHeaders headers = new HttpHeaders();
+            headers.set(HttpHeaders.AUTHORIZATION, authorizationHeader);
+            headers.setAccept(List.of(MediaType.APPLICATION_JSON));
 
-        HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
+            HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
 
-        ResponseEntity<User> response = restTemplate.exchange(
-                "http://localhost:8085/api/v1/auth/authorize", HttpMethod.GET, requestEntity, User.class
-        );
+            ResponseEntity<User> response = restTemplate.exchange(
+                    "http://localhost:8085/api/v1/auth/authorize", HttpMethod.GET, requestEntity, User.class
+            );
 
-        return response.getBody();
+            return response.getBody();
+        }   catch (HttpClientErrorException | HttpServerErrorException e) {
+            System.out.println("HTTP Error: " + e.getStatusCode());
+            System.out.println("Error Response Body: " + e.getResponseBodyAsString());
+            throw e;
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Unexpected error occurred", e);
+        }
     }
 
 }
